82b2ec6047356d14983ae56c5f5f7575ee426107
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-10-22 21:40:12 +0530

    Added job routing for cloudlaunch pulsar

--- destinations.py
+++ destinations.py
@@ -1,6 +1,55 @@
-from galaxy.jobs import JobDestination
 import os
 
-def remote_pulsar_runner(job):
-    token = "this"
-    return JobDestination(runner="pulsar", params={"url": "http://127.0.0.1:8913", "private_token": token})
+from galaxy.jobs import JobDestination
+
+from cloudlaunch_cli.api.client import APIClient
+
+
+# Global variable for tracking round-robin index
+current_server_index = 0
+
+
+def _get_cloudlaunch_client(app):
+    # Obtain from app.config later
+    cloudlaunch_url = os.environ.get('CLOUDLAUNCH_API_ENDPOINT',
+                                     'http://localhost:8000/api/v1')
+    cloudlaunch_token = os.environ.get('CLOUDLAUNCH_API_TOKEN')
+    return APIClient(cloudlaunch_url, token=cloudlaunch_token)
+
+
+def _get_pulsar_servers(app):
+    """
+    Returns an array of tuples, consisting of the pulsar url and auth token
+
+    TODO: Add a caching implementation so that the API is not repeatedly
+    queried on each job dispatch
+    """
+    client = _get_cloudlaunch_client(app)
+    server_list = []
+    # TODO: Filter deployments by app name
+    for deployment in client.deployments.list():
+        launch_data = deployment.launch_task.result.get('pulsar', {})
+        # TODO: Remove this if when filtering implemented
+        if launch_data and launch_data.get('api_url'):
+            server_list.append(
+                (launch_data.get('api_url'), launch_data.get('auth_token')))
+    return server_list
+
+
+def _get_next_pulsar_server(app):
+    """
+    Round-robin implementation for returning next available pulsar server
+    """
+    global current_server_index
+    servers = _get_pulsar_servers(app)
+    if current_server_index >= len(servers):
+        current_server_index = 0
+    next_server = servers[current_server_index]
+    current_server_index += 1
+    return next_server
+
+
+def remote_pulsar_runner(app):
+    url, token = _get_next_pulsar_server(app)
+    return JobDestination(runner="pulsar",
+                          params={"url": url, "private_token": token})
